8c5a01018c9be9d338b510e3f9a33d6b6e12422c
Geovani Pereira <geovani@localhost.localdomain>
2020-09-25 09:33:49 -0300

    model product

--- controller/product.go
+++ controller/product.go
@@ -1,10 +1,30 @@
 package controller
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 )
+
+// ProductTemplateVariables variables for template parser
+type ProductTemplateVariables struct {
+	Title string
+}
 
 // Product controller
 func Product(w http.ResponseWriter, r *http.Request) {
 
+	variables := ProductTemplateVariables{
+		Title: "Todos produtos",
+	}
+
+	pageTemplate, err := template.ParseFiles("view/product.html")
+	if err != nil {
+		log.Print("Template parsing error: ", err)
+	}
+
+	err = pageTemplate.Execute(w, variables)
+	if err != nil {
+		log.Print("Template executing error: ", err)
+	}
 }
