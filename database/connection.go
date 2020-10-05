1e96bceddb62cb67d5c8a913f638b8c78e3aa5c6
geovani-moc <geovani.moc@outlook.com>
2020-10-05 15:40:49 -0300

    atualizado a conexao com o postgres

--- database/connection.go
+++ database/connection.go
@@ -3,36 +3,38 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	"log"
-	"os"
 
-	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+//Quando for colocado em produção mudar
+//essa informações para um arquivo .env
+const (
+	host         = "localhost"
+	port         = 5432
+	user         = "postgres"
+	password     = "postgres"
+	databaseName = "gcommerce"
 )
 
 //create connection with postgres db
 func createConnection() *sql.DB {
-	//load .env file
-	err := godotenv.Load(".env")
+	postgresURL := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, databaseName)
 
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-
-	//Open the connection
-	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+	db, err := sql.Open("postgres", postgresURL)
 
 	if err != nil {
 		panic(err)
 	}
 
-	//check the connection
 	err = db.Ping()
-
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("Successfully connected.")
-	//return the connection
+
 	return db
 }
