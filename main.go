cfc5de928640576b4fb0fc52818f5d91dd1c284f
geovani-moc <geovani.moc@outlook.com>
2020-11-03 11:04:41 -0300

    iniciado interpretacao de argumentos

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/geovani-moc/gcommerce/actions"
 )
@@ -11,10 +13,22 @@ tambem comecar os testes
 ----------------------------------------------------------*/
 
 func main() {
+	arguments := os.Args
+
+	if len(arguments) > 1 {
+		execute(arguments[1:])
+	}
+
 	app := actions.BuildApp() // adicionar sub dominios par usar os idiomas
 	err := app.Run()          // verificar problema de varios idiomas rodando ao mesmo tempo
 
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func execute(commands []string) {
+	for _, command := range commands {
+		fmt.Println(command)
 	}
 }
