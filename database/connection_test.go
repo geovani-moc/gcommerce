5eb9b513a6858c2817f79f5b3dd62ff852406c2a
geovani-moc <geovani.moc@outlook.com>
2020-11-10 10:55:42 -0300

    adicionado teste de criaçao de insert

--- database/connection_test.go
+++ database/connection_test.go
@@ -18,3 +18,18 @@ func TestCreateInsert_1(t *testing.T) {
 		t.Errorf("CreateInsert de 1 linha\ngot: %v\nwant: %v\n", got, want)
 	}
 }
+
+func TestCreateInsert_3(t *testing.T) {
+	columns := []string{
+		"id",
+		"nome",
+		"descricao",
+	}
+
+	got := CreateInsert(3, "pessoa", columns)
+	want := "insert into pessoa (id, nome, descricao) values($1,$2,$3,$4,$5,$6,$7,$8,$9)"
+
+	if got != want {
+		t.Errorf("CreateInsert de 1 linha\ngot: %v\nwant: %v\n", got, want)
+	}
+}
